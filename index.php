ac76132783975ee078e0aa426dcaab0c8a11a7fb
insanekinge <insanekinge@icloud.com>
2023-01-16 16:13:46 +0300

    another changes

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   $x = 10;
   $y = 20;
 
-  echo $x + $y. '<br>';
+  echo $x + $y. '<br>'; 
   echo $x - $y. '<br>';
   echo $x * $y. '<br>';
   echo $x / $y. '<br>';
@@ -27,14 +27,14 @@
   $x++; // $x +=1;
   $y--; // $y -=1;
 
-  echo M_PI. '<br>';
-  echo M_E. '<br>';
+  echo M_PI. '<br>'; //число пи
+  echo M_E. '<br>'; //экспанента
 
   echo abs(-22). '<br>';
   echo round(3.55566734, 2). '<br>';
-  $rand = mt_rand(1, 20);
+  $rand = mt_rand(1, 20);       // выводит рандомное число из промежутка данных
   $rand++;
-  cho $rand. '<br>';
+  echo $rand . '<br>';
 
   echo max(2, 5, 1, 8, 43, 6, 13). '<br>';
 
@@ -44,34 +44,43 @@
   echo "<input type=\"text\"><br>";
 
   $length = strlen($str);
-  echo strtoupper(trim("  some  "));
+  echo strtoupper(trim("  some  "));  // убирает пробелы
   echo $length;
 
-  echo md5("qwerty");
+  echo md5("qwerty"); 
 
   //условные конструкции
   $a = 5;
-  $str == "Hello"
-  $isWeatherGood = false;
+  $str == "Hello";
+  $isWeatherGood = false; //задали переменнную и добавляем её в условие
   
   // if($a == 5) {
   //   echo '$a = 5';
   // } 
 
     if($str == "Hello" || $isWeatherGood == false) {
+
       echo 'first';
+
     } else if($a == 5) {
+
       $res = "second";
-      eco $res;
+      echo $res;
+
       if($str == "Hello") {
         echo "<br> yes";
       }
+      
     }
+
     else if ('$a >50'){
     echo '$a > 50';
+
     } 
+
     else if ('$a < 45'){
-      echo '$a <45'
+      echo '$a <45';
+
     }
     
     else {                //ELSE - дополнительная конструкция условного оператора
@@ -91,12 +100,15 @@
         case 7:
           echo "Var: 7";
           break;
+
         case 9:
           echo "Var: 9";
           break;  
+
         case 9:
           echo "Var: 3";
           break;
+          
         default:                //ключевое слово типо else/ выполняется в том случае, если предыдущие условия не сработали.
           echo "Default work!";    
     }
